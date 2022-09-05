011f9f93946dc542ba06127770a0570a790cd228
codesenseAye <44715949+codesenseAye@users.noreply.github.com>
2022-09-05 17:09:44 -0500

    title screen
    added text title
    added some anims

---
+++ Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject[] planes;
+    public static GameObject playB;
+
+    public static void Restart()
+    {
+        GameObject menu = GameObject.Find("Menu");
+        GameObject title = menu.transform.Find("Title").gameObject;
+        title.GetComponent<Animator>().SetBool("MenuState", true);
+
+        title.GetComponent<TMP_Text>().text = "PLANE GAME";
+        playB.SetActive(true);
+    }
+
+    public void DoStart()
+    {
+        GameObject plane = DoPickPlane();
+        GameObject title = transform.Find("Title").gameObject;
+
+        TMP_Text c_text = title.GetComponent<TMP_Text>();
+        c_text.text = plane.name.Substring(0, plane.name.Length - 7);
+
+        title.GetComponent<Animator>().SetBool("MenuState", false);
+
+        if (playB == null)
+        {
+            playB = transform.Find("Play").gameObject;
+        }
+
+        playB.SetActive(false);
+
+        CameraController cam = GameObject.Find("Main Camera").GetComponent<CameraController>();
+
+        cam.plane = plane;
+    }
+
+    private GameObject DoPickPlane()
+    {
+        int planeNum = Random.Range(0, planes.Length);
+        GameObject plane = Instantiate(planes[planeNum]);
+
+        plane.transform.SetParent(null);
+        return plane;
+    }
+}
